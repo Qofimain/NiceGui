bc941978be7b660b443280422cf5c2cd7803f13c
Qofimain <xcomdaewi@gmail.com>
2024-06-12 13:45:03 +0000

    Reactive

--- main.py
+++ main.py
@@ -15,31 +15,58 @@ from nicegui import ui
 
 url1="https://people.sc.fsu.edu/~jburkardt/data/csv/hw_200.csv"
 df = pd.read_csv(url1,header=0,names=['Index', 'Height', 'Weight'], usecols = [0,1,2])
-df = df.assign(BMI = (df['Weight'] * 0.4535)/((df['Height'] * 0.0254)**2)).round(2)
+
+#https://www.cdc.gov/nccdphp/dnpao/growthcharts/training/bmiage/page5_2.html#:~:text=Formula%3A%20weight%20(lb)%20%2F,in)%5D2%20x%20703&text=Then%2C%20calculate%20BMI%20by%20dividing,a%20conversion%20factor%20of%20703.
+#Formula: weight (lb) / [height (in)]2 x 703
+df['BMI'] = round(df.Weight / df.Height**2*703,1)
+
 print(df)
-# df.to_excel('f.xlsx')
 
 def update(*, df: pd.DataFrame, r: int, c: int, value):
-    df.iat[r, c] = value
-    ui.notify(f'Set ({r}, {c}) to {value}')
+    df.iat[r, c] = value #обновляем вес и рост в DF
+
+    newbmi = round(df.iat[r,2] / df.iat[r,1]**2*703,1) #вычисляем BMI c обновленными значениями
+    df.iat[r,3]=newbmi #сохраняем обновленный BMI в DF
+
+    bmi_dict[r].content=str(newbmi) #находим в словаре HTML-объект BMI по строке, в к-й обновлялся вес или рост
+                                    #и заменяем его контент
+    df.to_excel('f.xlsx') #обновляем файл
 
 
-with ui.grid(rows=len(df.index)+1).classes('grid-flow-col'):
+ui.button('Загрузить файл Excel', on_click=lambda: ui.download('f.xlsx'))
+sb=df.BMI
+print(df.iloc[[0],[3]]['BMI'][0])
+
+
+
+bmi_dict={} #словарь HTML-объектов BMI
+
+# with ui.grid(rows=len(df.index)+1).classes('grid-flow-col mx-8 mt-8'):
+#     for c, col in enumerate(df.columns):
+#         ui.label(col).classes('font-bold')
+#         for r, row in enumerate(df.loc[:, col]):
+#             if is_bool_dtype(df[col].dtype):
+#                 cls = ui.checkbox
+#                 print(cls)
+#             elif is_numeric_dtype(df[col].dtype):
+#                 cls = ui.number
+#                 print(cls)
+#             else:
+#                 cls = ui.input
+#             cls(value=row, on_change=lambda event, r=r, c=c: update(df=df, r=r, c=c, value=event.value))
+
+
+with ui.grid(rows=len(df.index)+1).classes('grid-flow-col mx-1 mt-1 items-center'):
     for c, col in enumerate(df.columns):
         ui.label(col).classes('font-bold')
         for r, row in enumerate(df.loc[:, col]):
-            if c == len(df.columns) - 1:
-                print("Massage", df.iloc[r, -1])
-                cls1 = ui.html(f"abc {df.iloc[r, -1]}")
-            elif is_bool_dtype(df[col].dtype):
-                cls = ui.checkbox
-            elif is_numeric_dtype(df[col].dtype):
+            if c == 0 :
+                ui.html(f'{df.iloc[r,c]}',tag='div') #индексы
+            elif c == 3: #BMI
+                bmi = ui.html(f'{df.iloc[r,c]}').classes('font-semibold text-blue-600 ') #HTML-объект BMI
+                bmi_dict[r] = bmi #сохраняем в словаре с ключем текущей строки
+            elif c in (1, 2): #рост и вес
                 cls = ui.number
-            else:
-                cls = ui.input
-                
+                cls(value=row,on_change=lambda event, r=r, c=c: update(df=df, r=r, c=c, value=event.value))
 
-
-            cls(value=row, on_change=lambda event, r=r, c=c: update(df=df, r=r, c=c, value=event.value))
-
-ui.run()
+ui.run(uvicorn_logging_level='info')
